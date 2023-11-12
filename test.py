c19c763201c110124909c91fd9507b46764557f8
SpaceBa13 <brayanalpizar36@gmail.com>
2023-11-11 18:11:16 -0600

    Se agregaron los demas metodos de prueba

--- test.py
+++ test.py
@@ -1,15 +1,41 @@
 import pytest
 import Examen2
 
-# Segunda Prueba para el método ObtieneValencia
+# Prueba para el método ObtieneValencia
 def test_obtiene_valencia():
+    mi_clase = Examen2.MiClase(None, None, None, None, None)
+    resultado = mi_clase.ObtieneValencia(55522)
+    assert resultado == 3
+
+# Prueba para el método DivisibleTempo
+def test_divisible_tempo():
+    mi_clase = Examen2.MiClase(None, None, None, None, None)
+    resultado = mi_clase.DivisibleTempo(15)
+    assert resultado == [1, 3, 5, 15]
+
+
+# Prueba para el método ObtieneMasBailable
+def test_obtiene_mas_bailable():
+    mi_clase = Examen2.MiClase(None, None, None, None, None)
+    resultado = mi_clase.ObtieneMasBailable([0.2, 0.1, 0.5, 0.3])
+    assert resultado == 0.5
+
+
+# Prueba para el método VerificaListaCanciones
+def test_verifica_lista_canciones():
+    mi_clase = Examen2.MiClase(None, None, None, None, None)
+    resultado = mi_clase.VerificaListaCanciones(["Rock Thing", "Deadlocked", "Hydra"])
+    assert resultado
+
+# Segunda Prueba para el método ObtieneValencia
+def test_obtiene_valencia2():
     mi_clase = Examen2.MiClase(None, None, None, None, None)
     resultado = mi_clase.ObtieneValencia(10088)
     assert resultado == 1
 
 
 # Segunda Prueba Unitaria para el método DivisibleTempo
-def test_divisible_tempo():
+def test_divisible_tempo2():
     mi_clase = Examen2.MiClase(None, None, None, None, None)
     resultado = mi_clase.DivisibleTempo(45)
     assert resultado == [1, 3, 5, 9, 15, 45]
